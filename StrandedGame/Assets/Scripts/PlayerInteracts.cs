210f507fcdbbebcecbbe7a1593be9969a49aa954
Kasper Lundberg <kasperlundberg1@gmail.com>
2023-02-14 14:08:46 +0100

    Interaction item
    added interaction to items + box collider to boat

---
+++ StrandedGame/Assets/Scripts/PlayerInteracts.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteracts : MonoBehaviour
+{
+    private void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.E)){
+        float interactRange = 2f;
+        Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange); 
+        foreach(Collider collider in colliderArray){
+            if(collider.TryGetComponent(out ItemInteractable itemInteractable)){
+                itemInteractable.Interact();
+            }
+        }
+        }
+        
+    }
+
+    public ItemInteractable GetInteractableObject()
+    {
+        float interactRange = 2f;
+        Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange); 
+        foreach(Collider collider in colliderArray){
+            if(collider.TryGetComponent(out ItemInteractable itemInteractable)){
+                return itemInteractable;
+            }
+        }
+        return null;
+    }
+}
