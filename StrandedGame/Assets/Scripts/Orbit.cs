df3067a292d3f207549cf888928e283d3fd83e1f
qorux <91456201+qorux@users.noreply.github.com>
2023-02-16 17:19:30 +0100

    sun now moves

---
+++ StrandedGame/Assets/Scripts/Orbit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public float theta = 0.1f;
+    void Update()
+    {
+        this.transform.RotateAround(Vector3.zero, Vector3.right, theta);
+    }
+}
