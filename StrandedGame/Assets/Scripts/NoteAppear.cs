d8ccf917e4efff9d2e816aac2b175a8e08c6599b
qorux <91456201+qorux@users.noreply.github.com>
2023-03-02 10:42:25 +0100

    Updated note texts

--- StrandedGame/Assets/Scripts/NoteAppear.cs
+++ StrandedGame/Assets/Scripts/NoteAppear.cs
@@ -39,9 +39,9 @@ public class NoteAppear : MonoBehaviour
     {
 
         noteCount = slot.AmountInSlot;
-        if (noteCount==55)
-        {  
-            noteText.text = "I�m sorry I left you here on the island, but I need you to retrieve something for me. Somewhere on the island a very valuable treasure is hidden. I was here many years ago searching for it but could not find it, and now I am too old to do it myself!\r\n\r\nOh and sorry about the broken boat, it was the only one I could spare. There should be some spare parts around the island for you to find. Last time I was here there were some rum smugglers operating on the eastern part of the island, you might find something useful there. (FYI the boat points to the north). I have left some notes around the island that should help you find what you need to. I also left you a shovel that might come in handy.\r\n\r\nGood luck! //X";
+        if (noteCount == 55)
+        {
+            noteText.text = "I�m sorry I left you here on the island, but I need you to retrieve something for me. Somewhere on the island a very valuable treasure is hidden. I was here many years ago searching for it but could not find it, and now I am too old to do it myself!\r\n\r\n There are some spare parts around the island for you to find in order to fix the boat. At each location I have left a note for you, to help you find the next one. I also left you a shovel that might come in handy. \r\n\r\n For your first clue - I remember there were some rum smugglers operating on the eastern part of the island a while back, you might find something useful there. (FYI the boat points to the north).\r\n\r\n Good luck! //X";
         }
         else if (noteCount == 56)
         {
@@ -49,10 +49,10 @@ public class NoteAppear : MonoBehaviour
         }
         else if (noteCount == 57)
         {
-            
-            noteText.text = "You will probably need some spare planks in order to fix the boat. If you carry on on the left path, you�ll get to the lagoon. Here, the planks can be found where the tall palm trees cross.\r\n\r\n//X";
+
+            noteText.text = "You will probably need some spare planks in order to fix the boat. If you carry on on the western path, you�ll get to the lagoon. Here, the planks can be found where the tall palm trees cross.\r\n\r\n//X";
         }
-        else if (noteCount==58)
+        else if (noteCount == 58)
         {
             noteText.text = "Now, you should have everything you need to fix the boat, time to get the treasure!\r\n\r\nLike I said I never did manage to get to the treasure last time I was here� The treasue is said to be deep in the cave, and the key is hidden somewhere on the island. I did find a clue as to where the key is, perhaps you�ll have more luck with it.\r\n\r\nThe clue is as follows:\r\n\r\n�Under the cliff, you�ll find it in a jif\r\n\r\nBy the palm dead, search the white head�";
         }
